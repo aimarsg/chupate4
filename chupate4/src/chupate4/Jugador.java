60946b2b1ff60b8d99a5ef10a52189c3fc8de6d3
Adrian <cuadron11@gmail.com>
2021-04-23 14:12:57 +0200

    jugador

--- chupate4/src/chupate4/Jugador.java
+++ chupate4/src/chupate4/Jugador.java
@@ -1,6 +1,51 @@
 package chupate4;
 
 public class Jugador {
-	private int id;
-	private String tula;
+	private int nombre;
+	private ListaCartas mano;
+	private int idJugador;
+	
+	
+	//constructora
+	public Jugador(int pNombre, int pIdJugador) {
+		this.nombre= pNombre;
+		this.idJugador= pIdJugador;
+		this.mano= new ListaCartas();
+	}
+	
+	//otros metodos
+	
+	public void cogerCarta(int pCuantas) {
+		int i = 1;
+		Carta carta;
+		while (i<=pCuantas) {
+			carta= Baraja.getMiBaraja().robar;
+			this.mano.anadirCarta(carta);
+		}
+		
+	}
+	
+	public void tirarCarta() {
+
+		Carta carta;
+		carta= this.mano.buscarCarta(Teclado.getMiTeclado().leerString());
+		if (carta.sePuedeEchar()) {
+			Baraja.getMiBaraja().echarCarta(carta);
+			this.mano.eliminarCarta(carta);
+		}
+	}
+	
+	// completar
+	public void decirUno() {
+		
+	}
+	
+	public void escribirMano() {
+		this.mano.escribirCartas();
+	}
+	
+	public int cantidadCartas() {
+		return(this.mano.cantidadCartas());
+	}
+	
 }
