0842b430b5618264714a0d7c2842700ddd9cc8e6
media <aimar.s.737@gmail.com>
2021-04-25 19:38:49 +0200

    Clase Baraja: metodos actualizados

--- chupate4/src/chupate4/Baraja.java
+++ chupate4/src/chupate4/Baraja.java
@@ -1,7 +1,65 @@
 package chupate4;
 
 public class Baraja {
+	//Atributos
 	private ListaCartas listaInicial;
 	private ListaCartas listaEchadas;
 	private static Baraja miBaraja=null;
+	//Constructora
+	private Baraja() {
+        this.listaEchadas= new ListaCartas();
+        this.listaInicial= new ListaCartas();
+    }
+	//Metodos
+    public static Baraja getMiBaraja() {
+        if(Baraja.miBaraja == null) {
+            Baraja.miBaraja=new Baraja();
+        }
+        return(Baraja.miBaraja);
+    }
+	
+	public void repartir() {
+		ListaJugadores.getMiListaJugadores().repartir();
+	}
+	public void anadirJugadores() {
+		boolean correcto=false;
+		int cuantos=0;
+		System.out.println("Escribe cuantos jugadores van a jugar la partida: ");
+		System.out.println("**El numero de jugadores de la prtida debe ser entre 2 y 10.**");
+		//este while es para pedir un numero valido
+		while (!correcto) {
+			cuantos=Teclado.getMiTeclado().leerEntero();
+			if (cuantos>=2 && cuantos<=10) {
+				correcto=true;
+			}else {//
+				System.out.println("El número introducido no es válido, por favor introduce otro");
+			}
+		}
+		//este while es para añadir los jugadores
+		int cont=0;
+		while (cont<cuantos) {
+			System.out.println("Escribe el nombre del jugador "+(cont+1));
+			String nombre=Teclado.getMiTeclado().leerString();
+			Jugador j=new Jugador(nombre, cont);
+			ListaJugadores.getMiListaJugadores().anadirJugador(j);
+		}
+	}
+	
+    public void barajear() {
+        this.listaInicial.mezclar();
+    }
+
+    public void darVueltaCartas() {
+        ListaCartas listaAux;
+        Carta ulti;
+
+        ulti=this.listaEchadas.ultimaCarta();
+        listaAux=listaInicial;
+        listaInicial=listaEchadas;
+        listaInicial.mezclar();
+        listaEchadas=listaAux;
+
+        this.listaEchadas.anadirCarta(ulti);
+        this.listaInicial.eliminarCarta(ulti);
+    }
 }
