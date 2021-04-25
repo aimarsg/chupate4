4cc3b732c91bb2d2a0e721b1a06f419e1a3b81b9
aleja <aleja@DESKTOP-16UTKFF>
2021-04-25 14:36:10 +0200

    La prueba de la Carta Normal la he hecho para probar lo de esrcibir la carta, pero hasta que no estén todas las clases hechas no voy a seguirla

---
+++ chupate4/Pruebas/chupate4/CartaNormalTest.java
@@ -0,0 +1,25 @@
+package chupate4;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CartaNormalTest {
+	CartaNormal c1;
+	@BeforeEach
+	void setUp() throws Exception {
+		c1=new CartaNormal("azul",8);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testEscribirCarta() {
+		c1.escribirCarta();
+	}
+
+}
--- chupate4/src/chupate4/CartaNormal.java
+++ chupate4/src/chupate4/CartaNormal.java
@@ -23,10 +23,111 @@ public class CartaNormal extends Carta{
 	}
 	
 	public void escribirCarta() {
-		if(this.getColor()=="azul") {
+		String azul = "\u001B[34m";
+		String rojo="\u001B[31m";
+		String amarillo="\u001B[33m";
+		String verde="\u001B[32m";
+		String cero[]={" ##### ", // H=0
+					   " #   # ", // H=1
+                       " #   # ", // H=2
+                       " #   # ", // H=3
+                       " #   # ", // H=4
+                       " #   # ", // H=5
+                       " ##### "};// H=6
+
+ String uno[]={"   #   ",
+               "  ##   ",
+               "   #   ",
+               "   #   ",
+               "   #   ",
+               "   #   ",
+               " ##### "};
+
+ String dos[]={" ##### ",
+               "     # ",
+               "     # ",
+               " ##### ",
+               " #     ",
+               " #     ",
+               " ##### "};
+
+ String tres[]={" ##### ",
+                "     # ",
+                "     # ",
+                " ##### ",
+                "     # ",
+                "     # ",
+                " ##### "};
+
+ String cuatro[]={" #   # ",
+                  " #   # ",
+                  " #   # ",
+                  " ##### ",
+                  "     # ",
+                  "     # ",
+                  "     # "};
+
+ String cinco[]={" ##### ",
+                 " #     ",
+                 " #     ",
+                 " ##### ",
+                 "     # ",
+                 "     # ",
+                 " ##### "};
+
+ String seis[]={" ##### ",
+                " #     ",
+                " #     ",
+                " ##### ",
+                " #   # ",
+                " #   # ",
+                " ##### "};
+
+ String siete[]={" ##### ",
+                 "     # ",
+                 "     # ",
+                 "  #### ",
+                 "     # ",
+                 "     # ",
+                 "     # "};
+
+ String ocho[]={ " ##### ",
+                 " #   # ",
+                 " #   # ",
+                 " ##### ",
+                 " #   # ",
+                 " #   # ",
+                 " ##### "};
+
+ String nueve[]={" ##### ",
+                 " #   # ",
+                 " #   # ",
+                 " ##### ",
+                 "     # ",
+                 "     # ",
+                 "     # "};
+ 
+		 
 			if(this.numero==0) {
-				
-			}
+				System.out.println(this.getColor()+cero);
+				}else if(this.numero==1) {
+					System.out.println(this.getColor()+uno);
+				}else if(this.numero==2) {
+					System.out.println(this.getColor()+dos);	
+				}else if(this.numero==3) {
+					System.out.println(this.getColor()+tres);
+				}else if(this.numero==4) {
+					System.out.println(this.getColor()+cuatro);
+				}else if(this.numero==5) {
+					System.out.println(this.getColor()+cinco);
+				}else if(this.numero==6) {
+					System.out.println(this.getColor()+seis);
+				}else if(this.numero==7) {
+					System.out.println(this.getColor()+siete);
+				}else if(this.numero==8) {
+					System.out.println(this.getColor()+ocho);
+				}else if(this.numero==9) {
+					System.out.println(this.getColor()+nueve);
 		}
 	}
 }
