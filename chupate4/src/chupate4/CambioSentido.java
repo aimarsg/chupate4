92ebf0b1a9ef8f994ad2f9986bbd08ee32d1c61e
IKER <ikerfuertes23@gmail.com>
2021-04-27 12:45:49 +0200

    cambio sentido done

--- chupate4/src/chupate4/CambioSentido.java
+++ chupate4/src/chupate4/CambioSentido.java
@@ -1,5 +1,22 @@
 package chupate4;
 
-public class CambioSentido extends {
-
+public class CambioSentido extends EspecialesColor{
+	public CambioSentido(String pColor) {
+		super(pColor);
+	}
+	public void tirarCarta() {
+		ListaJugadores.getMiListaJugadores().cambiarSentido();
+	}
+	public void escribirCarta() {
+		
+	}
+	public boolean sePuedeEchar() {
+		boolean sePuede=true;
+		if(!super.sePuedeEchar()) {
+			if (Baraja.getMiBaraja().ultimaCarta() instanceof CambioSentido) {
+				sePuede=true;
+			}
+		}
+		return(sePuede);
+	}
 }
