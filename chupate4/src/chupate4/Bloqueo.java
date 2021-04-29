3541500d97bf2c8a2ef54c32d124a763a0934bf1
media <aimar.s.737@gmail.com>
2021-04-29 19:57:25 +0200

    bloqueo y cambio sentido

--- chupate4/src/chupate4/Bloqueo.java
+++ chupate4/src/chupate4/Bloqueo.java
@@ -14,6 +14,7 @@ public class Bloqueo extends EspecialesColor{
 	}
 	
 	public void tirarCarta() {
+		this.escribirCarta();
 		String nombre=ListaJugadores.getMiListaJugadores().siguienteJugador().getNombre();
 		System.out.println("Se ha saltado a "+nombre);
 		ListaJugadores.getMiListaJugadores().saltarTurno();
